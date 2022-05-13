dba13d3fa0b6582a53b9e6fda9b4582aa8f4c8ff
omar96developer <omakino@hotmail.it>
2022-05-13 21:00:26 +0200

    create function for save the todo item

--- js/script.js
+++ js/script.js
@@ -9,38 +9,58 @@ $(document).ready(function (){
         $('.todos').prepend(todo);
 
         createInput(todo.children('.text'));
+        saveTodo();
 
     });
 
-    function createInput(todo){
-        const textTodo = $(todo).children('.todo__text').text();
-        const input = $('.template--text').children().clone();
+    
+  });
 
-        todo.children('.todo__text').html(input);
-        
-       
-        input.val(textTodo).focus().focusout(function () {
+
+
+  function createInput(todo){
+      const textTodo = $(todo).children('.todo__text').text();
+      const input = $('.template--text').children().clone();
+
+      todo.children('.todo__text').html(input);
+      
+     
+      input.val(textTodo).focus().focusout(function () {
+        const input = $(this);
+
+        if(textTodo.trim().length > 0){
+          input.parent('.todo__text').html(textTodo);
+        } else {
+          input.parents('.todos__item').remove();
+        }
+      })
+      
+     .keyup(function (event) {
           const input = $(this);
   
-          if(textTodo.trim().length > 0){
+          if(event.keyCode === 27 && textTodo.trim().length > 0) {
             input.parent('.todo__text').html(textTodo);
-          } else {
+          } else if (event.keyCode === 27 && textTodo.trim().length === 0){
             input.parents('.todos__item').remove();
           }
-        })
-        
-       .keyup(function (event) {
-            const input = $(this);
-    
-            if(event.keyCode === 27 && textTodo.trim().length > 0) {
-              input.parent('.todo__text').html(textTodo);
-            } else if (event.keyCode === 27 && textTodo.trim().length === 0){
-              input.parents('.todos__item').remove();
-            }
-          }); 
-    }
-    /* 2 read */
-    /* 3 update */
-    /* 4 delete */
-
-});
+        }); 
+  }
+  
+  function saveTodo() {
+    //save on enter
+    $(document).on( 'keypress', '.todo__text__input', function (event) {
+      if(event.keyCode === 13) {
+        const input = $(this);
+        if(input.val().trim().length > 0) {
+          input.parent('.todo__text').html(input.val());
+        } else {
+          input.parents('.todos__item').remove();
+        }
+      }
+    });
+  }
+  
+ 
+  /* 2 read */
+  /* 3 update */
+  /* 4 delete */
